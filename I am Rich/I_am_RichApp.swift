0498d5da3541df3ebf06e6abb55fb735df76c6f4
ckost9n <ckost9n@gmail.com>
2022-04-27 11:25:22 +0300

    Initial Commit

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Konstantin on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
